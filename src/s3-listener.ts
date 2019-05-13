77fc6b7b6e990e98cab4a0a76f1ad75aecf292ee
Ruslan Gainutdinov <ruslan.gainutdinov@futurice.com>
2019-05-14 00:53:36 +0300

    Filter events processed

--- src/s3-listener.ts
+++ src/s3-listener.ts
@@ -35,30 +35,36 @@ export const s3Handler = (
     if (s3Event.Records) {
       s3Event.Records.forEach(s3Record => {
         const fileName = s3Record.s3.object.key
-        const s3Url = 's3://' + s3Record.s3.bucket.name + '/' + fileName
-        const name = path.basename(fileName, ext(fileName))
-        const userId = name.substring(0, name.lastIndexOf('-'))
-        logger.info(`Starting workflow for s3Url=${s3Url}, userId=${userId}`)
-        return workflow
-          .startExecution({
-            stateMachineArn: config.WORKFLOW_ARN,
-            name: uuidV4(),
-            input: JSON.stringify({
-              s3Url,
-              userId,
-            }),
-          })
-          .promise()
-          .then(workflowStartResult => {
-            logger.warn('Execution started', workflowStartResult)
-            apiResponse(event, context, callback).success(workflowStartResult)
-          })
-          .catch(workflowStartError => {
-            logger.warn('Failed to start workflow', workflowStartError)
-            apiResponse(event, context, callback).failure(
-              'Failed to start workflow: ' + workflowStartError.message,
-            )
-          })
+        if (fileName.endsWith('.json')) {
+          logger.info('Skipping JSON file', s3Record)
+        } else if (fileName.indexOf('-thumbnail') > 0) {
+          logger.info('Skipping thumbnail file', s3Record)
+        } else {
+          const s3Url = 's3://' + s3Record.s3.bucket.name + '/' + fileName
+          const name = path.basename(fileName, ext(fileName))
+          const userId = name.substring(0, name.lastIndexOf('-'))
+          logger.info(`Starting workflow for s3Url=${s3Url}, userId=${userId}`)
+          return workflow
+            .startExecution({
+              stateMachineArn: config.WORKFLOW_ARN,
+              name: uuidV4(),
+              input: JSON.stringify({
+                s3Url,
+                userId,
+              }),
+            })
+            .promise()
+            .then(workflowStartResult => {
+              logger.warn('Execution started', workflowStartResult)
+              apiResponse(event, context, callback).success(workflowStartResult)
+            })
+            .catch(workflowStartError => {
+              logger.warn('Failed to start workflow', workflowStartError)
+              apiResponse(event, context, callback).failure(
+                'Failed to start workflow: ' + workflowStartError.message,
+              )
+            })
+        }
       })
     }
   } catch (err) {
